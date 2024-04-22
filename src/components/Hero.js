8ac2c459ee551c5826ab983b5dc0b133a71e084e
Doston <icreateuz@gmail.com>
2024-04-22 23:31:48 +0500

    fixing issues

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,35 @@ import MainImg from "../assets/images/hero-img.png"
 import { useState , useEffect} from 'react';
 import axios from 'axios';
 
+const translateRes = {
+ ["tomato late blight"]:"Pomidor kech hastalanishi",
+ ["tomato leaf mold"]: "Pomidor Barglari Mog'orlari",
+ ["tomato mosaic birus"]:"Pomidor Mozaikasi Virusi",
+ ["tomato septoria leaf spot"]:  "Pomidor Septoriyasi",
+ ["apple black rot"]: "Olma Qora Chirishi",
+ ["apple healthy"]:"Sog'lom olma",
+ ["apple apple scab"]:"Olma Qoraqo'tiri",
+ ["cherry healthy"]:"Sog'lom gilos",
+ ["cherry powdery mildrew"]:"Gilos Kukunli Mildri",
+ ["corn gray leaf spot"]:"Makkajo'xori Kulrang Bargli Joy",
+ ["corn healthy"]:"Sog'lom Makkajo'xori",
+ ["corn rust"]:"Makkajo'xori Zang",
+ ["grape black rot"]:"Uzum Qora Chirishi",
+ ["grape Esca Black Measles"]:"Uzum Eska Qora Qizamiq",
+ ["grape Healthy"]:"Sog'lom Uzum",
+ ["grape Leaf Blight Isariopsis Leaf Spot"]:"Uzum Barglari Izariopsis kassaligi",
+ ["peach bactorial spot"]:"Shaftoli Bakteriali",
+ ["peach healthy"]: "Sog'lom Shaftoli",
+ ["pepper bell healthy"]:"Qalampir Qo'ng'irog'i Sog'lom",
+ ["potato early blight"]:"Qalampir Qo'ng'irog'i Erta Kassalanishi",
+ ["potato healthy"]:"Sog'lom kartoshka",
+ ["potato late blight"]: "Kartoshka kech kassalanishi",
+ ["strawberry healthy"]:"Sog'lom Qulupnay",
+ ["strawberry leaf scorch"]:"Qulupnay Barglari Kuyishi",
+ ["tomato bacterial spot"]:"Pomidor bakteriali",
+ ["tomato early blight"]: "Pomidor erta kassalanishi",
+ ["tomato healthy"]:"Sog'lom Pomidor",
+}
 
 const Hero = () => {
     const [selectedFile, setSelectedFile] = useState()
@@ -75,11 +104,11 @@ const Hero = () => {
                     </div>
                     {selectedFile && !uploading &&  
                         <div className='flex gap-10 max-w-[500px] w-full'>
-                            <button className='rounded-full text-primary  text-2xl bg-white  h-16 w-full hover:bg-primary hover:text-white' onClick={onDelete}>Delete</button> 
-                            <button className='rounded-full text-white hover:bg-primary   text-2xl bg-primary  h-16 w-full hover:bg-white hover:text-primary' onClick={onPredict}>{predicting ? "Predicting..." : "Predict"}</button>
+                            <button className='rounded-full text-primary  text-2xl bg-white  h-16 w-full hover:bg-primary hover:text-white' onClick={onDelete}>Oʻchirish</button> 
+                            <button className='rounded-full text-white hover:bg-primary   text-2xl bg-primary  h-16 w-full hover:bg-white hover:text-primary' onClick={onPredict}>{predicting ? "Baholanmoqda..." : "Baholash"}</button>
                         </div> 
                     }
-                   {prediction ? (<p className="text-[32px] text-white">{prediction}</p>) : ""} 
+                   {prediction ? (<p className="text-[32px] text-white">{translateRes[prediction.toLowerCase()] || prediction}</p>) : ""} 
                 </div>
 
 
